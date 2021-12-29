88d9fe5e625d6728f300ad6a8f90de62cd10ef21
Greezlyyy <87074517+Greezlyyy@users.noreply.github.com>
2021-12-29 12:49:21 +0100

    Aggiunta della home

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual è il cibo preferito di Yuri?",
+    answer: "Kebab",
+    options: [
+      "Pizza",
+      "Kebab",
+      "McDonald's",
+      "Pasta al sugo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quanti anni ha Yuri?",
+    answer: "11",
+    options: [
+      "10",
+      "9",
+      "11",
+      "6"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual è l'animale preferito di Yuri?",
+    answer: "Gatto",
+    options: [
+      "Cane",
+      "Gatto",
+      "Coniglio",
+      "Furetto"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Cosa preferisce Yuri?",
+    answer: "Montagna",
+    options: [
+      "Mare",
+      "Piscina",
+      "Montagna",
+      "Dormire"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual è la materia preferita di Yuri?",
+    answer: "Inglese",
+    options: [
+      "Inglese",
+      "Francese",
+      "Matematica",
+      "Scienze"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
